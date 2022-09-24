a535761cda8778cc4e426dd594c2b0100a6168b2
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-24 13:11:46 +0300

    changing database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,8 +1,41 @@
 CREATE DATABASE razborkawork;
-CREATE TABLE ebaytab(
+CREATE TABLE manualtab(
+    id SERIAL NOT NULL PRIMARY KEY,
+    device VARCHAR(170) NOT NULL,
+    oem VARCHAR(300) NOT NULL,
+    analog_oem VARCHAR(300),
+    link VARCHAR(300),
+    image VARCHAR(300),
+    datetime DATE
+    );
+CREATE TABLE allsitetab(
     id SERIAL NOT NULL PRIMARY KEY,
     model VARCHAR(50),
     country VARCHAR(50),
+    device VARCHAR(170) NOT NULL,
+    oem VARCHAR(100) NOT NULL,
+    count VARCHAR(300),
+    price VARCHAR(300),
+    link VARCHAR(300),
+    image VARCHAR(300),
+    datetime DATE
+    );
+CREATE TABLE ebaytab(
+    id SERIAL NOT NULL PRIMARY KEY,
+    model VARCHAR(50),
+    country VARCHAR(50) DEFAULT Germany,
+    device VARCHAR(170) NOT NULL,
+    oem VARCHAR(100) NOT NULL,
+    count_ebay VARCHAR(300),
+    price_ebay VARCHAR(300),
+    link VARCHAR(300),
+    image VARCHAR(300),
+    datetime VARCHAR(300)
+    );
+CREATE TABLE bampertab(
+    id SERIAL NOT NULL PRIMARY KEY,
+    model VARCHAR(50),
+    country VARCHAR(50) DEFAULT Belarus,
     device VARCHAR(170) NOT NULL,
     oem VARCHAR(100) NOT NULL,
     count_ebay VARCHAR(300),
@@ -14,30 +47,18 @@ CREATE TABLE ebaytab(
     datetime VARCHAR(300)
     );
 
--- CREATE DATABASE work;
+
 -- CREATE TABLE ebaytab(
 --     id SERIAL NOT NULL PRIMARY KEY,
 --     model VARCHAR(50),
---     country VARCHAR(50),
+--     country VARCHAR(50) DEFAULT Belarus,
 --     device VARCHAR(170) NOT NULL,
 --     oem VARCHAR(100) NOT NULL,
---     count_ebay INTEGER,
---     price_ebay DECIMAL(8,2),
---     price_store DECIMAL(8,2),
+--     count_ebay VARCHAR(300),
+--     price_ebay VARCHAR(300),
+--     price_store VARCHAR(300),
 --     count_store VARCHAR(10),
 --     link VARCHAR(300),
---     image VARCHAR(300)
---     );
-
-
-
--- CREATE DATABASE perntodo;
---
--- CREATE TABLE todo(
---     todo_id SERIAL PRIMARY KEY,
---     description VARCHAR(255),
---     completed boolean
+--     image VARCHAR(300),
+--     datetime VARCHAR(300)
 -- );
---
--- ALTER TABLE todo
--- ADD COLUMN completed BOOLEAN DEFAULT FALSE;
