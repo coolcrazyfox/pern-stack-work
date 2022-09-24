1f89bbf905f0adc98f458f90d681b443e18f194c
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-24 13:19:50 +0300

    database.sql

--- server/database.sql
+++ server/database.sql
@@ -4,8 +4,8 @@ CREATE TABLE manualtab(
     device VARCHAR(170) NOT NULL,
     oem VARCHAR(300) NOT NULL,
     analog_oem VARCHAR(300),
-    link VARCHAR(300),
-    image VARCHAR(300),
+    link TEXT,
+    image TEXT,
     datetime DATE
     );
 CREATE TABLE allsitetab(
@@ -16,8 +16,8 @@ CREATE TABLE allsitetab(
     oem VARCHAR(100) NOT NULL,
     count VARCHAR(300),
     price VARCHAR(300),
-    link VARCHAR(300),
-    image VARCHAR(300),
+    link TEXT,
+    image TEXT,
     datetime DATE
     );
 CREATE TABLE ebaytab(
@@ -28,9 +28,9 @@ CREATE TABLE ebaytab(
     oem VARCHAR(100) NOT NULL,
     count_ebay VARCHAR(300),
     price_ebay VARCHAR(300),
-    link VARCHAR(300),
-    image VARCHAR(300),
-    datetime VARCHAR(300)
+    link TEXT,
+    image TEXT,
+    datetime DATE
     );
 CREATE TABLE bampertab(
     id SERIAL NOT NULL PRIMARY KEY,
@@ -40,11 +40,33 @@ CREATE TABLE bampertab(
     oem VARCHAR(100) NOT NULL,
     count_ebay VARCHAR(300),
     price_ebay VARCHAR(300),
-    price_store VARCHAR(300),
-    count_store VARCHAR(10),
-    link VARCHAR(300),
-    image VARCHAR(300),
-    datetime VARCHAR(300)
+    link TEXT,
+    image TEXT,
+    datetime DATE
+    );
+CREATE TABLE avitatab(
+    id SERIAL NOT NULL PRIMARY KEY,
+    model VARCHAR(50),
+    country VARCHAR(50) DEFAULT Russia,
+    device VARCHAR(170) NOT NULL,
+    oem VARCHAR(100) NOT NULL,
+    count VARCHAR(300),
+    price VARCHAR(300),
+    link TEXT,
+    image TEXT,
+    datetime DATE
+    );
+CREATE TABLE storetab(
+    id SERIAL NOT NULL PRIMARY KEY,
+    model VARCHAR(50),
+    country VARCHAR(50) DEFAULT Belarus,
+    device VARCHAR(170) NOT NULL,
+    oem VARCHAR(100) NOT NULL,
+    count VARCHAR(300),
+    price VARCHAR(300),
+    link TEXT,
+    image TEXT,
+    datetime DATE
     );
 
 
