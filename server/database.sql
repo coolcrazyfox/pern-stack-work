71a9a231787583e222750f47fddd545a9dd36535
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-27 19:47:13 +0300

    database

--- server/database.sql
+++ server/database.sql
@@ -5,44 +5,20 @@ CREATE TABLE oem(
     )
 CREATE TABLE manualtab(
     id SERIAL NOT NULL PRIMARY KEY,
-    oem_id INTEGER,
-    model  VARCHAR(300) NOT NULL,
+    oem_id BIGINT,
+    model  VARCHAR(300),
     device VARCHAR(170) NOT NULL,
     oem VARCHAR(300) NOT NULL,
     analog_oem VARCHAR(300),
     link TEXT,
     image TEXT,
     datetime VARCHAR(50),
-    FOREIGN KEY (oem_id) REFERENCES oem(id)
-    );
-
-
-CREATE TABLE ebaytab(
-    id SERIAL NOT NULL PRIMARY KEY,
-    model VARCHAR(50),
-    country VARCHAR(50) ,
-    device VARCHAR(170) NOT NULL,
-    oem VARCHAR(100) NOT NULL,
-    count VARCHAR(300),
-    price VARCHAR(300),
-    link TEXT,
-    image TEXT,
-    datetime VARCHAR(50)
-    );
-CREATE TABLE bampertab(
-    id SERIAL NOT NULL PRIMARY KEY,
-    model VARCHAR(50),
-    country VARCHAR(50) ,
-    device VARCHAR(170) NOT NULL,
-    oem VARCHAR(100) NOT NULL,
-    count VARCHAR(300),
-    price VARCHAR(300),
-    link TEXT,
-    image TEXT,
-    datetime VARCHAR(50)
+    FOREIGN KEY (oem_id) REFERENCES oem(id) ON DELETE CASCADE
     );
 CREATE TABLE avitatab(
     id SERIAL NOT NULL PRIMARY KEY,
+    manual_id BIGINT,
+    name_site_id BIGINT,
     model VARCHAR(50),
     country VARCHAR(50) ,
     device VARCHAR(170) NOT NULL,
@@ -51,10 +27,46 @@ CREATE TABLE avitatab(
     price VARCHAR(300),
     link TEXT,
     image TEXT,
-    datetime VARCHAR(50)
+    datetime VARCHAR(50),
+    FOREIGN KEY (manual_id) REFERENCES manualtab(id) UNIQUE(manual_id),
+    FOREIGN KEY (name_site_id) REFERENCES namesite(id) ON DELETE CASCADE
+    );
+CREATE TABLE bampertab(
+    id SERIAL NOT NULL PRIMARY KEY,
+    manual_id BIGINT,
+    name_site_id BIGINT,
+    model VARCHAR(50),
+    country VARCHAR(50) ,
+    device VARCHAR(170) NOT NULL,
+    oem VARCHAR(100) NOT NULL,
+    count VARCHAR(300),
+    price VARCHAR(300),
+    link TEXT,
+    image TEXT,
+    datetime VARCHAR(50),
+    FOREIGN KEY (manual_id) REFERENCES manualtab(id) UNIQUE(manual_id),
+    FOREIGN KEY (name_site_id) REFERENCES namesite(id) ON DELETE CASCADE
+    );
+CREATE TABLE ebaytab(
+    id SERIAL NOT NULL PRIMARY KEY,
+    manual_id BIGINT,
+    name_site_id BIGINT,
+    model VARCHAR(50),
+    country VARCHAR(50) ,
+    device VARCHAR(170) NOT NULL,
+    oem VARCHAR(100) NOT NULL,
+    count VARCHAR(300),
+    price VARCHAR(300),
+    link TEXT,
+    image TEXT,
+    datetime VARCHAR(50),
+    FOREIGN KEY (manual_id) REFERENCES manualtab(id) UNIQUE(manual_id),
+    FOREIGN KEY (name_site_id) REFERENCES namesite(id) ON DELETE CASCADE
     );
 CREATE TABLE storetab(
     id SERIAL NOT NULL PRIMARY KEY,
+    manual_id BIGINT,
+    name_site_id BIGINT,
     model VARCHAR(50),
     country VARCHAR(50) ,
     device VARCHAR(170) NOT NULL,
@@ -63,20 +75,22 @@ CREATE TABLE storetab(
     price VARCHAR(300),
     link TEXT,
     image TEXT,
-    datetime VARCHAR(50)
+    datetime VARCHAR(50),
+    FOREIGN KEY (manual_id) REFERENCES manualtab(id) UNIQUE(manual_id),
+    FOREIGN KEY (name_site_id) REFERENCES namesite(id) ON DELETE CASCADE
     );
 CREATE TABLE allsitetab(
-    id SERIAL NOT NULL PRIMARY KEY,
-    model VARCHAR(50),
-    country VARCHAR(50),
-    device VARCHAR(170) NOT NULL,
-    oem VARCHAR(100) NOT NULL,
-    analog_oem VARCHAR(300),
-    count VARCHAR(300),
-    price VARCHAR(300),
-    link TEXT,
-    image TEXT,
-    datetime VARCHAR(50)
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     model VARCHAR(50),
+--     country VARCHAR(50),
+--     device VARCHAR(170) NOT NULL,
+--     oem VARCHAR(100) NOT NULL,
+--     analog_oem VARCHAR(300),
+--     count VARCHAR(300),
+--     price VARCHAR(300),
+--     link TEXT,
+--     image TEXT,
+--     datetime VARCHAR(50)
     );
 CREATE TABLE namesite(
     id SERIAL NOT NULL PRIMARY KEY,
